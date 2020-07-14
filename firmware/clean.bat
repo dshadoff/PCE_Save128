3d22eea6a04f7276e54fe512c85dc4d01c36a1fa
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-07-14 00:49:16 -0400

    Firmware skeleton project
    Add files necessary to compile a project in Diamond
    This is just a 'blinky' project at the moment

---
+++ firmware/clean.bat
@@ -0,0 +1,14 @@
+del *~
+del *.bak
+del *.orig
+del *.html
+rmdir /S /Q Save128_tcr.dir
+rmdir /S /Q MachXO2_1200\Save128_MachXO2_1200.dir
+rmdir /S /Q MachXO2_1200\.vdbs
+del /Q MachXO2_1200\Save128_MachXO2_1200*.*
+del MachXO2_1200\*.html
+del MachXO2_1200\*.log
+del MachXO2_1200\xxx_lse_*
+del /Q MachXO2_1200\TinyFPGA_A2_*.*
+del MachXO2_1200\message.xml
+
