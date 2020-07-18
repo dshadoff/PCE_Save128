fff90bbac7ddf312e41e2f9d86d31bd22286c05e
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-07-18 14:57:48 -0400

    Add more project files to ignore

--- firmware/clean.bat
+++ firmware/clean.bat
@@ -1,13 +1,18 @@
 del *~
 del *.bak
+del *.ccl
 del *.orig
 del *.html
+del *.spread_sheet.*
+del *.spreadsheet*
+del *Untitled.tpf
 rmdir /S /Q Save128_tcr.dir
 rmdir /S /Q MachXO2_1200\Save128_MachXO2_1200.dir
 rmdir /S /Q MachXO2_1200\.vdbs
 del /Q MachXO2_1200\Save128_MachXO2_1200*.*
 del MachXO2_1200\*.html
 del MachXO2_1200\*.log
+del MachXO2_1200\Untitled*.*
 del MachXO2_1200\xxx_lse_*
 del /Q MachXO2_1200\TinyFPGA_A2_*.*
 del MachXO2_1200\message.xml
