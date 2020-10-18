09961fb5ee3ad2ab6b04cf6d1f91fee5b85e627e
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-10-18 18:43:01 -0400

    Get SPI working...sort of
    SPI read function roks for first read; subsequent accesses need to be debugged.

--- firmware/Save128.v
+++ firmware/Save128.v
@@ -14,8 +14,8 @@ module top (
   //inout pin13_tdi,
   //inout pin14_tck,
   //inout pin15_tms,
-  inout pin16,
-  inout pin17,
+  inout pin16, 
+  input wire pin17,
   inout pin18_cs,
   inout pin19_sclk,
   input wire pin20_miso,
@@ -29,7 +29,7 @@ module top (
 //  assign pin3_sn = 1'bz;
 //  assign pin4_mosi = 1'bz;
 //  assign pin5 = 1'bz;
-  assign pin6 = 1'bz;
+//  assign pin6 = 1'bz;
 //  assign pin7_done = 1'bz;
 //  assign pin8_pgmn = 1'bz;
   
@@ -39,7 +39,7 @@ module top (
   //assign pin14_tck = 1'bz;
   //assign pin15_tms = 1'bz;
   assign pin16 = 1'bz;
-  assign pin17 = 1'bz;
+//  assign pin17 = 1'bz;
 //  assign pin18_cs = 1'bz;
 //  assign pin19_sclk = 1'bz;
 //  assign pin20_miso = 1'bz;
@@ -48,59 +48,71 @@ module top (
 
   wire clk;
   OSCH #(
-    .NOM_FREQ("2.08")			// 53.20 is OK (see page 29 of PLL guide)
+    .NOM_FREQ("53.20")			// 53.20 is OK (see page 29 of PLL guide)  2.08
   ) internal_oscillator_inst (
     .STDBY(1'b0),
 	.OSC(clk)
   );
   
-  reg [31:0] led_timer = 8'h00000000;
+//  reg [31:0] led_timer = 8'h00000000;
 
   reg init = 0;
   reg spidrv_clk = 0;
   reg spidrv_clk_prev = 0;
   reg [1:0] sp_phase = 2'b00;
-  reg [22:0] spi_uberclock = 0;
+  reg [23:0] spi_uberclock = 0;
   reg sp_clk;
-  reg sp_cs = 0;
-  reg sp_hold = 0;
+  reg sp_cs_n = 0;
+  reg sp_hold_n = 0;
   reg sp_go   = 1;
   reg sp_active = 0;
   reg sp_end_desel = 0;
   reg sp_mosi = 0;
   reg sp_miso = 0;
-  reg  [5:0] cmd_len = 6'b000011;  // 3;
-  reg  [3:0] rw_len = 0;
-  reg [31:0] cmd_buf = 32'b11110000_00000000_00000000_00000000; // = { 3'b101 , 29'b0};
-  reg  [7:0] rd_buf = 8'b00000000;
-  reg  [7:0] wr_buf = 8'b00000000;
+  reg  [5:0] cmd_len = 6'b100000;  // 32;
+  reg  [4:0] rw_len = 5'b01000;  // 8
+  reg [31:0] cmd_buf = 32'b00000011_00000000_00000000_00000000; // = { SPI 3 = READ; 24 bit addr = 0 };
+  reg  [15:0] rd_buf = 16'b00000000;
+  reg  [15:0] wr_buf = 16'b00000000;
   
+//  always @(posedge clk)
+//	begin
+//		led_timer <= led_timer[30:0] +2;
+//	end
+//
   always @(posedge clk)
 	begin
-		led_timer <= led_timer[30:0] +2;
-	end
 
-  always @(posedge clk)
-	begin
-
-		if (spi_uberclock[1] && !init)
+		if (spi_uberclock[10] && !init)
 		  begin
-			sp_cs   <= 1'b1;
-			sp_hold <= 1'b1;
+			sp_cs_n   <= 1'b1;
+			sp_hold_n <= 1'b0;
 			init    <= 1'b1;
 
-			cmd_buf[31:29] <= 3'b101;
-			cmd_len        <= 3;
-			wr_buf[7:5]   <= 3'b010;
-			rw_len         <= 3;
-			sp_end_desel   <= 1;
+			cmd_buf[31:24] <= 8'b00000011; // _00000000_00000000_00000000;
+			cmd_len        <= 32;
+			wr_buf[7:0]   <= 8'b00000000;
+			rw_len         <= 8;
+			sp_end_desel   <= 0;
+			sp_go          <= 1'b1;
+		  end
+		  
+		if (spi_uberclock[10] && !sp_active && !pin17)
+		  begin
+//			cmd_buf[31:0] <= 8'b00000011_00000000_00000000_00000001;
+//			cmd_len        <= 32;
+
+			wr_buf[7:0]   <= 8'b00000000;
+			rd_buf[7:0]   <= 8'b00000000;
+			rw_len         <= 8;
+			sp_end_desel   <= 0;
 			sp_go          <= 1'b1;
 		  end
 
 		spi_uberclock  <= spi_uberclock + 1'b1;
         sp_phase[1:0] <= spi_uberclock[22:21];
 
-		spidrv_clk     <= led_timer[22];
+		spidrv_clk     <= spi_uberclock[21];
 		sp_miso        <= pin20_miso;
 
 		if (init && (spi_uberclock[20:0] == 21'b00000_00000000_00000000))
@@ -112,21 +124,21 @@ module top (
 			if (sp_phase == 2'b00)
 			  begin
 //				// if active, set data on MOSI
-//				// if start transmission, sp_cs <= 0; sp_hold <= 0;
-				if (sp_active && sp_cs)
+//				// if start transmission, sp_cs_n <= 0; sp_hold_n <= 1;
+				if (sp_active && sp_cs_n)
 				  begin
-					sp_cs <= 0;
-					sp_hold <= 0;
+					sp_cs_n <= 0;
+					sp_hold_n <= 1;
 				  end
 
 				if (sp_active)
 				  begin
+					sp_hold_n <= 1;
 					if (cmd_len > 0)
 					  begin
 						sp_mosi       <= cmd_buf[31];
 						cmd_buf[31:1] <= cmd_buf[30:0];
 						cmd_buf[0]    <= 1'b0;
-//						cmd_len <= cmd_len - 1'b1;
 					  end
 					else if (rw_len > 0)
 					  begin
@@ -135,6 +147,15 @@ module top (
 						wr_buf[0]   <= 1'b0;
 					  end
 				  end
+
+				// if end transmission, sp_cs_n <= 1 *and/or* sp_hold_n <= 0
+				else // if (!sp_active)
+				  begin
+					sp_hold_n <= 0;
+					if (sp_end_desel)
+					  sp_cs_n <= 1;
+				  end
+
 			  end
 			  
 			  
@@ -181,39 +202,36 @@ module top (
 			  begin
 				sp_clk <= 0;
 
-//				// if end transmission, sp_cs <= 1 *and/or* sp_hold <= 1
-				if (!sp_active)
-				  begin
-					sp_hold <= 1;
-					if (sp_end_desel)
-					  sp_cs <= 1;
-				  end
 			  end
 			  			
 
 		  end
 	end
 
-  assign pin1       = led_timer[24];
-  assign pin2       = rd_buf[7];
+  assign pin1       = spi_uberclock[23];
+
+  assign pin2       = sp_phase[1];
+  assign pin3_sn    = sp_phase[0];
+
 //  assign pin7_done  = led_timer[23];
 //  assign pin8_pgmn  = led_timer[22];
 //  assign pin9_jtgnb = led_timer[21];
-  assign pin10_sda  = led_timer[20];
-  assign pin11_scl  = led_timer[19];
 
-  assign pin7_done  = rw_len[1];
-  assign pin8_pgmn  = rw_len[0];
-  assign pin9_jtgnb = cmd_buf[31];
-	
-  assign pin18_cs   = sp_cs;
+  assign pin18_cs   = sp_cs_n;
   assign pin19_sclk = sp_clk;
 //  assign pin20_miso = sp_miso;
   assign pin21      = sp_mosi;
-  assign pin22      = sp_hold;
+  assign pin22      = sp_hold_n;
 
-  assign pin5       = spidrv_clk;
-  assign pin3_sn    = sp_phase[1];
-  assign pin4_mosi  = sp_phase[0];
+  
+  assign pin4_mosi  = rd_buf[7];
+  assign pin5       = rd_buf[6];
+  assign pin6       = rd_buf[5];
+  assign pin7_done  = rd_buf[4];
+  assign pin8_pgmn  = rd_buf[3];
+  assign pin9_jtgnb = rd_buf[2];
+  assign pin10_sda  = rd_buf[1];
+  assign pin11_scl  = rd_buf[0];
+	
 
 endmodule
